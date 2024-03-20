745857ec65e16ac9775e4ec0fea63b01ac39dd36
vuson1709 <thaison170996@gmail.com>
2024-03-20 14:00:44 +0700

    feat: BE - Implement Login

--- server/index.js
+++ server/index.js
@@ -4,10 +4,17 @@ import mongoose from "mongoose";
 dotenv.config();
 import { UserRouter } from "./routes/user.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+  origin: "http://localhost:3006",
+  credentials: true,
+};
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use("/auth", UserRouter);
 
 mongoose.connect("mongodb://127.0.0.1:27017/authentication");
